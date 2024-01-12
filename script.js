ae11c9d725b35f1da9cdf97eb0e36732a227b07f
kamranifti <kamranifti030@gmail.com>
2024-01-12 02:33:56 -0800

    about me picture changed

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-      let isDark = true 
-      let body = document.querySelector ('body');
-      let sun = document.getElementById("sun");
-      let moon = document.getElementById("moon");
+let isDark = true
+let body = document.querySelector('body');
+let sun = document.getElementById("sun");
+let moon = document.getElementById("moon");
 
-      sun.onclick = function(){
-       document.body.classList.toggle("light-theam");
+sun.onclick = function () {
+      document.body.classList.toggle("light-theam");
 
-       if (isDark == true) {
-        sun.classList.remove('fa-toggle-on')
-        sun.classList.add('fa-toggle-off')
-        moon.classList.remove('fa-sun')
-        moon.classList.add('fa-moon')
-        isDark = false
-       } 
-       
-       else {
-        sun.classList.remove('fa-toggle-of')
-        sun.classList.add('fa-toggle-on') 
-        moon.classList.add('fa-sun')
-        moon.classList.remove('fa-moon')
-        isDark = true
-       }
+      if (isDark == true) {
+            sun.classList.remove('fa-toggle-on')
+            sun.classList.add('fa-toggle-off')
+            moon.classList.remove('fa-sun')
+            moon.classList.add('fa-moon')
+            isDark = false
       }
-      
+
+      else {
+            sun.classList.remove('fa-toggle-of')
+            sun.classList.add('fa-toggle-on')
+            moon.classList.add('fa-sun')
+            moon.classList.remove('fa-moon')
+            isDark = true
+      }
+}
